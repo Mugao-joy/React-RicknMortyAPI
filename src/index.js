679a8051ea66cb64dee7d3d7847be461041baf00
Mugao-joy <mugaojoy@gmail.com>
2024-02-27 16:54:44 +0300

    added paths

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import CharacterList from './components/CharacterList';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
+import CharacterDetails from './components/CharacterDetails';
+//import App from './App';
+import NavBar from './components/Navbar';
+
+
+const routes = createBrowserRouter([
+  {
+    path: '/home',
+    element: <Home/>
+  },
+  {
+    path: '/characters',
+    element: <CharacterList/>
+  },
+  {
+    path: '/details',
+    element: <CharacterDetails/>
+  },
+  {
+    path: '/notfound',
+    element: <NotFound/>
+  },
+  {
+    path: '/',
+    element: <NavBar/>
+  }
+])
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {routes}/>
   </React.StrictMode>
 );
 
