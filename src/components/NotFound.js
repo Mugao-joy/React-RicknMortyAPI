de90cd6d40a619d0055b616fe957be87543d194b
Mugao-joy <mugaojoy@gmail.com>
2024-02-27 16:49:34 +0300

    added a notfound file for 404 errors

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+    return(
+        <div>
+            <h2>Page Not Found</h2>
+            <p>Sorry, its not you its us! we couldn't find the page you're looking for</p>
+        </div>
+    )
+}
+
+export default NotFound
